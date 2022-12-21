193fbaecc72b00d10fc317373af89f44ae950f57
kairat <63288004+Kairat77@users.noreply.github.com>
2022-12-21 14:53:26 +0600

    first commit

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,29 +1,29 @@
 
-import {useState} from 'react'
-function Form(){
-    const [name,setName]=useState("");
-    const [tnc,setTnc]=useState(false);
-    const [interest,setInterest]=useState("");
+import React, { useState } from 'react';
+
+function Form() {
+  const [name,setName]=useState("");
+  const [isChecked, setIsChecked] = useState(false);
   function getFormData(e)
   {
-    console.warn(name,tnc,interest)
+    alert(isChecked, name)
     e.preventDefault()
   }
-    return(
-    <div className='center'>
-     <h1>Регистрация</h1>
-     <form onSubmit={getFormData}>
-       <input type="text" placeholder="имя" value={name} onChange={(e)=>setName(e.target.value)} /> <br /><br />
-       <select onChange={(e)=>setInterest(e.target.value)}>
-         <option>холост</option>
+
+  return (
+    <div>
+      <form onSubmit={getFormData}>
+      <input type="text" placeholder="имя" value={name} onChange={(e)=>setName(e.target.value)} /> <br /><br />
+      <select onChange={(e)=>setInterest(e.target.value)}>
+      <option>холост</option>
          <option>женат</option>
          <option>замужем</option>
-       </select> <br /><br />
-       <input type="checkbox"  onChange={(e)=>setTnc(e.target.checked)} /><span>Я согласен с определенным условием</span>
-       <br /><br />
-       <button type="submit">Отправить</button>
-     </form>
-     </div>
-    )
+      </select>
+      <input type="checkbox" checked={isChecked} onChange={() => setIsChecked(!isChecked)} />
+      <button disabled={!isChecked}>Нажмите меня</button>
+      </form>
+    </div>
+  );
 }
+
 export default Form
