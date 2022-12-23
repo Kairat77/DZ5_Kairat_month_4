a8566cf4a4234fdae354399f6cde58cf74e37344
kairat <63288004+Kairat77@users.noreply.github.com>
2022-12-23 13:16:42 +0600

    first commit

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -11,16 +11,16 @@ function Form() {
   }
 
   return (
-    <div>
+    <div className='box'>
       <form onSubmit={getFormData}>
-      <input type="text" placeholder="имя" value={name} onChange={(e)=>setName(e.target.value)} /> <br /><br />
-      <select onChange={(e)=>setInterest(e.target.value)}>
-      <option>холост</option>
-         <option>женат</option>
-         <option>замужем</option>
-      </select>
-      <input type="checkbox" checked={isChecked} onChange={() => setIsChecked(!isChecked)} />
-      <button disabled={!isChecked}>Нажмите меня</button>
+        <input disabled={isChecked} type="text" placeholder="имя" value={name} onChange={(e)=>setName(e.target.value)} /> <br /><br />
+          <select disabled={isChecked} onChange={(e)=>setInterest(e.target.value)}>
+            <option>холост</option>
+            <option>женат</option>
+            <option>замужем</option>
+          </select>
+        <input type="checkbox" checked={isChecked} onChange={() => setIsChecked(!isChecked)} />
+        <button disabled={!isChecked}>Нажмите меня</button>
       </form>
     </div>
   );
